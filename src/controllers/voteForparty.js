f6b611251017ae82b0769f1e11c75fc73b521fe5
Hpeter1988 <hpeter1988@msn.com>
2019-08-01 19:07:52 +0200

    FEAT(dev) new vote added endpoint finished

--- src/controllers/voteForparty.js
+++ src/controllers/voteForparty.js
@@ -1,10 +1,12 @@
 const voteForParty = require('../services/voteForParty/voteForParty-service');
+const updatePartyRating = require('../services/voteForParty/updateOverallRatingForParty-service');
 
 const voteForPartyController = (req, res) => {
   const { partyname } = req.headers;
   const { vibeRating, crowdRating, musicRating } = req.body;
 
   voteForParty.setRatings(partyname, vibeRating, crowdRating, musicRating)
+    .then(partyInfo => updatePartyRating.updatePartyOverallRating(partyInfo))
     .then((data) => {
       res.status(200).json(data);
     })
--- src/services/voteForParty/voteForParty-service.js
+++ src/services/voteForParty/voteForParty-service.js
@@ -3,6 +3,7 @@ const Voter = require('../../models/voter');
 
 const setRatings = (partyName, vibeRating, crowdRating, musicRating) => new Promise((resolve, reject) => {
   const newVotes = new Voter({ vibeRating, crowdRating, musicRating });
+
   Party.findOneAndUpdate({ partyName }, {
     $push: {
       voters: newVotes,
@@ -13,7 +14,23 @@ const setRatings = (partyName, vibeRating, crowdRating, musicRating) => new Prom
     if (err) {
       reject(err);
     } else {
-      resolve(partyData);
+      const allVotes = partyData.voters;
+      let allVibeRating = 0;
+      let allCrowdRating = 0;
+      let allMusicRating = 0;
+
+      for (let i = 0; i < allVotes.length; i += 1) {
+        allVibeRating += allVotes[i].vibeRating;
+        allCrowdRating += allVotes[i].crowdRating;
+        allMusicRating += allVotes[i].musicRating;
+      }
+      const newVibeRating = Math.round(allVibeRating / allVotes.length);
+      const newCrowdRating = Math.round(allCrowdRating / allVotes.length);
+      const newMusicRating = Math.round(allMusicRating / allVotes.length);
+
+      resolve({
+        partyName: partyData.partyName, newVibeRating, newCrowdRating, newMusicRating, voterId: newVotes._id,
+      });
     }
   });
 });
