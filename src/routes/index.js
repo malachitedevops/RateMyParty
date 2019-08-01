4d1088ade4255e0885a07009ccfcc57d13a922c9
Hpeter1988 <hpeter1988@msn.com>
2019-08-01 22:33:58 +0200

    FEAT(dev) helloWorld mock files removed

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 
 const router = express.Router();
 
-const helloWorldController = require('../controllers/hello-world');
 const createPartyController = require('../controllers/createParty');
 const getPartyController = require('../controllers/getParty');
 const voteForPartyController = require('../controllers/voteForparty');
@@ -10,8 +9,6 @@ const updateVoteController = require('../controllers/updateCurrentVote');
 const deletePartyController = require('../controllers/endParty');
 const findPartyController = require('../controllers/findMyparty');
 
-
-router.get('/helloworld', helloWorldController.helloWorldController);
 router.post('/createparty', createPartyController.createPartyController);
 router.get('/party', getPartyController.findPartyController);
 router.post('/vote', voteForPartyController.voteForPartyController);
